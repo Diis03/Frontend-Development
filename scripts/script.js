6268903edfa17915bf30bac49ccd215ec924a0cf
Disna Yalcin <94454559+Diis03@users.noreply.github.com>
2022-01-18 15:44:02 +0100

    Add files via upload
    Ik heb mijn werk even opnieuw geüpload. Alle code die bij het hamburger menu hoorde, heb ik weggehaald en doe nu een nieuwe poging.

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,5 @@
 //Javascript
 
-// var hamburgerButton = document.querySelector("header button:first-of-type");
-// var navMenu = document.querySelector("header ul:first-of-type");
-
-// hamburgerButton.addEventListener("click", () => {
-    
-// }
 
 
 
