e81b70ecb4f6f20f0f93a4294244b61e43690e83
Disna Yalcin <94454559+Diis03@users.noreply.github.com>
2022-01-26 17:35:30 +0100

    Add files via upload
    - Media queries toegepast en website zoveel mogelijk responsive gemaakt.
    - Hamburger Menu doet het alleen op de eerste pagina en niet op de tweede.
    - Readme moet nog even bijgewerkt worden, maar dat gebeurt vanavond nog.

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,1 @@
-//Javascript
-
-var button = document.querySelector('header > nav button');
-var menu = document.querySelector('header > nav ul');
-
-button.addEventListener('click', buttonClick)
-
-function buttonClick() {
-	menu.classList.toggle('open-menu')
-}
-
-
-
+// JavaScript Document
