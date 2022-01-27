b526179bec0eebb43bb17ccce9565555bac03213
Disna Yalcin <94454559+Diis03@users.noreply.github.com>
2022-01-27 11:42:09 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
-// JavaScript Document
+//Javascript
+
+var button = document.querySelector('header > nav button');
+var menu = document.querySelector('header > nav ul');
+
+button.addEventListener('click', buttonClick)
+
+function buttonClick() {
+	menu.classList.toggle('open-menu')
+}
+
+
+
